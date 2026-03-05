33a6c372954e3fd97ff3aca5c0f15d986a0a27ba
nmarmi <nicolocmarmi@gmail.com>
2026-03-05 11:55:07 -0500

    added server entrypoint index.js

---
+++ server/index.js
@@ -0,0 +1,37 @@
+// Node APIs
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+
+// setup server
+dotenv.config()
+const PORT = process.env.PORT || 4000;
+const app = express()
+
+// setup middleware
+// there shouldnt be uploads >10mb
+app.use(express.urlencoded({ extended: true, limit: '10mb' })) 
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+}))
+app.use(express.json({ limit: '10mb' }))
+app.use(cookieParser())
+
+// init database
+const db = require('./db');
+
+(async () => {
+    try {
+        await db.init();  
+        console.log("db initialized");
+
+        app.listen(PORT, () => 
+            console.log(`Playlister Server running on port ${PORT}`)
+        );
+    } catch (err) {
+        console.error("Failed to initialize database:", err);
+    }
+})();
+
